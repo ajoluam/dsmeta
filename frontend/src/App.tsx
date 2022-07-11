1f761a272343ba71b2dfbb7926436e8ea8f5c851
Alexandre Amorim <aamorim@ciandt.com>
2022-07-11 17:56:56 -0300

    Ajuste App tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,23 +3,17 @@ import NotificationButton from './components/NotificationButton';
 import SalesCardComponent from './components/SalesCard';
 
 function App() {
-  return (
-    <>
-   
-    <HeaderComponent/>
-    <main>
-        <section id="sales">
-            <div className='dsmeta-container'>
-
-            </div>
-        </section>
-    </main>
-    <SalesCardComponent />
-    <NotificationButton/>
-    </>
-    
-  )
-
+    return (
+        <>
+            <HeaderComponent />
+            <main>
+                <section id='sales'>
+                    <div className='dsmeta-container'></div>
+                </section>
+            </main>
+            <SalesCardComponent />
+        </>
+    );
 }
 
 export default App;
